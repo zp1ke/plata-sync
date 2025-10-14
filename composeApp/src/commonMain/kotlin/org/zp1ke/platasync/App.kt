2dfd2ea8aafa188405359e4a6dd03f875ed71279
Matt Atcher <6463472+zp1ke@users.noreply.github.com>
2025-10-14 14:19:10 -0500

    Add custom theme and color support for PlataSync UI components

--- composeApp/src/commonMain/kotlin/org/zp1ke/platasync/App.kt
+++ composeApp/src/commonMain/kotlin/org/zp1ke/platasync/App.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
@@ -15,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.zp1ke.platasync.ui.theme.AppTheme
 
 import platasync.composeapp.generated.resources.Res
 import platasync.composeapp.generated.resources.compose_multiplatform
@@ -22,17 +24,20 @@ import platasync.composeapp.generated.resources.compose_multiplatform
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+    AppTheme {
         var showContent by remember { mutableStateOf(false) }
+        val colorScheme = MaterialTheme.colorScheme
         Column(
             modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
+                .background(colorScheme.background)
                 .safeContentPadding()
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+            Button(
+                colors = ButtonDefaults.outlinedButtonColors(containerColor = colorScheme.primaryContainer),
+                onClick = { showContent = !showContent }) {
+                Text("Click me!", color = colorScheme.onPrimaryContainer)
             }
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
@@ -41,7 +46,10 @@ fun App() {
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
                     Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+                    Text(
+                        text = "Compose: $greeting",
+                        color = colorScheme.onBackground
+                    )
                 }
             }
         }
--- composeApp/src/jvmMain/kotlin/org/zp1ke/platasync/Platform.jvm.kt
+++ composeApp/src/jvmMain/kotlin/org/zp1ke/platasync/Platform.jvm.kt
@@ -1,6 +1,6 @@
 package org.zp1ke.platasync
 
-class JVMPlatform: Platform {
+class JVMPlatform : Platform {
     override val name: String = "Java ${System.getProperty("java.version")}"
 }
 
