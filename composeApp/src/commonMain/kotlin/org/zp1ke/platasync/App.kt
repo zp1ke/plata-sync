e66ff2e9bcae0481f7fde7b374e7b825bcefcdd1
Matt Atcher <6463472+zp1ke@users.noreply.github.com>
2025-10-14 23:15:26 -0500

    Refactor App composable to simplify layout structure and remove unnecessary padding

--- composeApp/src/commonMain/kotlin/org/zp1ke/platasync/App.kt
+++ composeApp/src/commonMain/kotlin/org/zp1ke/platasync/App.kt
@@ -2,10 +2,7 @@ package org.zp1ke.platasync
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -13,25 +10,20 @@ import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.SlideTransition
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.zp1ke.platasync.ui.screen.AccountsScreen
-import org.zp1ke.platasync.ui.screen.CategoriesScreen
 import org.zp1ke.platasync.ui.theme.AppTheme
 
 @Composable
 @Preview
 fun App() {
     AppTheme {
-        Scaffold { innerPadding ->
-            Surface(
-                modifier = Modifier
-                    .background(MaterialTheme.colorScheme.background)
-                    .safeContentPadding()
-                    .padding(innerPadding)
-                    .fillMaxSize(),
-                color = MaterialTheme.colorScheme.background,
-            ) {
-                Navigator(AccountsScreen) { navigator ->
-                    SlideTransition(navigator)
-                }
+        Surface(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.background)
+                .fillMaxSize(),
+            color = MaterialTheme.colorScheme.background,
+        ) {
+            Navigator(AccountsScreen) { navigator ->
+                SlideTransition(navigator)
             }
         }
     }
