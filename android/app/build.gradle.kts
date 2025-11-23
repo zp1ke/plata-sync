c00c6779364b815ce1bc580c6581ff7360e52ec7
Matt Atcher <6463472+zp1ke@users.noreply.github.com>
2025-11-23 14:27:43 -0500

    feat: Update application ID and namespace to com.zp1ke.plata_sync across all platforms

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.plata_sync"
+    namespace = "com.zp1ke.plata_sync"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,10 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.plata_sync"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.zp1ke.plata_sync"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,8 +29,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
--- android/app/src/main/kotlin/com/zp1ke/plata_sync/MainActivity.kt
+++ android/app/src/main/kotlin/com/zp1ke/plata_sync/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.plata_sync
+package com.zp1ke.plata_sync
 
 import io.flutter.embedding.android.FlutterActivity
 
